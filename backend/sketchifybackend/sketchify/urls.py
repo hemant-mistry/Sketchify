9eb50a4ee279f8a94bec65c79b09896531415ee2
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-06-29 00:23:08 +0530

    Setting up APIs for Create and Join room.

--- backend/sketchifybackend/sketchify/urls.py
+++ backend/sketchifybackend/sketchify/urls.py
@@ -1,7 +1,7 @@
 # urls.py
 from django.urls import include, path
 from rest_framework import routers
-from .views import RedisTestView,CreateRoom,JoinRoom,DisplayAvailableRoom,SaveCanvasImage
+from .views import RedisTestView,CreateRoom,JoinRoom,SaveCanvasImage
 
 router = routers.DefaultRouter()
 
@@ -11,6 +11,5 @@ urlpatterns = [
     path('redis-test/', RedisTestView.as_view(), name='redis-test'),
     path('create-room/', CreateRoom.as_view(), name='create-room'),
     path('join-room/', JoinRoom.as_view(), name='join-room'),
-    path('display-rooms/', DisplayAvailableRoom.as_view(), name='display_rooms'),
     path('save_canvas_image/', SaveCanvasImage.as_view(), name='save_canvas_image'),
 ]
